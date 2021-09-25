db3a6dd5673c90064d669f09e52511350db6d388
ryanskystack <sadleopard@163.com>
2021-09-25 20:35:06 +1000

    Updates

---
+++ precache-manifest.e06fb613f70949a4902993d771c4868f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dcc0723e32d9e2ab33ef5952027b934",
+    "url": "/Twilio-call-application/index.html"
+  },
+  {
+    "revision": "97de0735d6bd30736438",
+    "url": "/Twilio-call-application/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "f66aab35654037741bb7",
+    "url": "/Twilio-call-application/static/js/2.512744f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Twilio-call-application/static/js/2.512744f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97de0735d6bd30736438",
+    "url": "/Twilio-call-application/static/js/main.93cbb055.chunk.js"
+  },
+  {
+    "revision": "8a946a712e79ca0009ac",
+    "url": "/Twilio-call-application/static/js/runtime-main.d5a188c0.js"
+  }
+]);
